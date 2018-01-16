8337b0305425ff9e2993fc212b0a8e30a1c6f4fd
Matthew The <matthewthe@gmail.com>
2018-01-16 12:17:49 +0100

    added E-step code

--- functions.py
+++ functions.py
@@ -1,22 +1,28 @@
 ﻿import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.special import digamma 
 
 def getDataDimensions(input_data):
     # gets the dimensions of the data tf
     # D Documents, N words
-    return (D,N)
+    return input_data.shape
 
+def loadMockData(D,V,max_repeats=5):
+    return np.random.randint(max_repeats, size=(D,V))
+
+#tf = loadData()
+tf = loadMockData(50, 100)
 
 # Initialize parameters
     
 # tf: input_data
 K = 10 # Number of Topics
-D = getDataDimensions(tf)[1] # Number of Documents
-N = getDataDimensions(tf)[2] # Number of words
+D = getDataDimensions(tf)[0] # Number of Documents
+V = getDataDimensions(tf)[1] # Number of words in dictionary
+N = np.max(np.sum(tf, axis = 1))
 beta_init = np.zeros((D, N)) # betas
-alpha_init = [50/K] * K # alphas
-gamma_init = [alpha[1] + N/K] * K # variational hypermeter of topic distribution
+alpha_init = [50.0/K] * K # alphas
+gamma_init = [alpha_init[0] + float(N)/K] * K # variational hypermeter of topic distribution
 phi_init = np.full((K, N), 1./K) # variational hypermeter of topics
 eta = 0.001 # fixed hyperparameter for the smoothed version
 Lambda_init = np.zeros((D, N)) # variational hyperparameter for smoothed version
@@ -32,10 +38,51 @@ def ComputeLikelihood(tf, alpha, beta, gamma, phi, Lambda):
     return(likelihood)
 
 
-def ExpectationStep(K, D, N, alpha, beta, gamma, phi, Lambda):
+def ExpectationStep(tf, K, D, N, alpha, beta, gamma, phi, Lambda):
     # Returns updated phi, gamma and likelihood L(γ,φ,λ; α,β)
-    return(phi, gamma, likelihood)
+    #
+    # k: topic (N.B.: r in Hazal's notes)
+    # d: document
+    # n: word in document
+    #
+    # γ: [document, topic], topic probabilities per document (Dirichlet)
+    # λ: [topic, word in dictionary], word emission probability per topic (Dirichlet)
+    # φ: [document, word in document, topic], word and topic probability for each word in the document (Multinomial)
+    # tf: [document, word in dictionary]
+    # α: [document], prior topic probability
+    # β: [topic], prior word probability (N.B.: eta in Hazal's notes!)
+    
+    for d in range(D):
+        wd = np.repeat(range(V), tf[d,:])
+        for n in range(len(wd)):
+            for k in range(K):
+                phi[d,n,k] = np.exp(digamma(gamma[d,k]) - digamma(np.sum(gamma[d,:])) + digamma(Lambda[k,wd[n]]) - digamma(np.sum(Lambda[k,:])))
+            phi[d,n,:] /= sum(phi[d,n,:])
+    
+    for d in range(D):
+        gamma[d,:] = alpha[d] + np.sum(phi[d,:,:], axis = 0)
+    
+    Lambda[k,:] = beta[k]
+    for d in range(D):
+        wd = np.repeat(range(V), tf[d,:])
+        for k in range(K):
+            for n in range(len(wd)):        
+                Lambda[k,wd[n]] += phi[d,n,k]
+    
+    #likelihood = ComputeLikelihood(tf, alpha, beta, gamma, phi, Lambda)
+    return(phi, gamma, Lambda, likelihood)
 
+def ExpectationStepUnitTest():
+    alpha = np.random.rand(D,1)
+    beta = np.random.rand(K,1)
+    gamma = np.random.rand(D,K)
+    phi = np.random.rand(D,N,K)
+    Lambda = np.random.rand(K,V)
+    
+    phi, gamma, Lambda, likelihood = ExpectationStep(tf, K, D, N, alpha, beta, gamma, phi, Lambda)
+    phi, gamma, Lambda, likelihood = ExpectationStep(tf, K, D, N, alpha, beta, gamma, phi, Lambda)
+
+ExpectationStepUnitTest()
 
 def updateAlpha(D, K, alpha, gamma):
     # Newton-Rhapson algorithm for updating alpha
