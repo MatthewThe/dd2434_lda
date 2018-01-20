718b9876b4d21d197c8f3d90fb4bdfbd55082cc5
Matthew The <matthewthe@gmail.com>
2018-01-20 17:40:52 +0100

    Added document classification, sort of works..

--- functions.py
+++ functions.py
@@ -17,17 +17,18 @@ import zipfile
 import os.path
 
 from sklearn import datasets, svm
-#from sklearn.model_selection import train_test_split
-#from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
 def main(argv):
     np.random.seed(1)
     #tf, labels_1, labels_2 = loadData('reutersdata', 'earn', 'grain')
     #tf = loadMockData(10, 10)
-    K = 3
+    K = 10
     
-    generatedData = True
+    print("*** LOADING DATA ***")
+    generatedData = False
     if generatedData:
         D = 50
         N = 15
@@ -40,13 +41,12 @@ def main(argv):
         print(theta_original)
         print("*** Original beta ***")
         print(beta_original)
-    #else:
-    #    tf, labels_1, labels_2, topic_texts = loadData('reutersdata', 'earn', 'grain')
+    else:
+        tf, labels_1, labels_2, topic_texts = loadData('reutersdata', 'earn', 'grain')
         #tf = loadSimpleData()
         #tf = csr_matrix(tf)
-    else:
-        filename = "data_CNA_notna.txt"
-        tf = loadCancerDataPosNeg(filename) # or tf=loadCancerDataAbsolute(filename) or tf=loadCancerDataBinary(filename)
+        #filename = "data_CNA_notna.txt"
+        #tf = loadCancerDataPosNeg(filename) # or tf=loadCancerDataAbsolute(filename) or tf=loadCancerDataBinary(filename)
     
     # Initialize parameters
 
@@ -55,16 +55,11 @@ def main(argv):
     D = getDataDimensions(tf)[0] # Number of Documents
     V = getDataDimensions(tf)[1] # Number of words in dictionary
     N = np.max(tf.sum(axis=1))
-    beta_init = np.zeros((D, N)) # beta
-    alpha_init = 50.0/K # alpha
-    gamma_init = [alpha_init + float(N)/K] * K # gamma
-    phi_init = np.full((K, N), 1./K) # phi
-    Lambda_init = np.zeros((D, N)) # lambda
-    loglikelihoodVEM = 0 # Likelihood to be minimized
 
     #ExpectationStepUnitTest()
     #MaximizationStepUnitTest()
     #VariationalExpectationMaximizationUnitTest(tf, D, K, V, N)
+    
     alpha = 5.0 / K
     gamma = np.ones((D,K)) * (alpha + float(N)/K)
     eta = 1.0 # np.random.rand() 
@@ -72,6 +67,7 @@ def main(argv):
     phi = np.ones((D,N,K)) * (1./ K)
     
     gamma, Lambda, phi = VariationalExpectationMaximization(tf, alpha, eta, gamma, phi, Lambda) 
+    #gamma = readResults()
     
     if generatedData:
         print("*** Original alpha: %.5f, Original eta: %.5f ***\n" % (alpha_original, eta_original))
@@ -84,6 +80,11 @@ def main(argv):
         print(compareGamma)
     else:
         printResults(gamma, topic_texts)
+        
+        mean_stats_low_dim, sd_stats_low_dim, mean_stats_high_dim, sd_stats_high_dim = DocumentClassification(gamma, tf, labels_1)
+        plotSVMModelAccuracy(mean_stats_low_dim, sd_stats_low_dim, mean_stats_high_dim, sd_stats_high_dim)
+        
+        #DocumentClassification(gamma, tf, labels_2)
     
 def printResults(gamma, topic_texts):
     D = len(topic_texts)
@@ -91,6 +92,12 @@ def printResults(gamma, topic_texts):
     for d in range(D):
         writer.writerow(list(gamma[d,:]) + [topic_texts[d]])
 
+def readResults():
+    reader = csv.reader(open('output.tsv', 'r'), delimiter = '\t')
+    gamma = list()
+    for row in reader:
+      gamma.append(map(float, row[:3]))
+    return np.array(gamma)
 ##### Start of Synthetic Data Generation #####
 def createAlpha(maxAlpha=1):
     alpha = np.random.rand() * maxAlpha
@@ -657,6 +664,8 @@ def MaximizationStepUnitTest():
 ### End of Maximization Part ###
 
 def VariationalExpectationMaximization(tf, alpha, eta, gamma, phi, Lambda, EstepConvergeThreshold = 1e-6, EstepMaxIterations = 100):
+    print("*** VARIATIONAL EM ***")
+    
     # Calculates variational parameters gamma, phi, and lambda iteratively until convergence
     likelihood = 10**(-10) 
     converged = False
@@ -769,63 +778,64 @@ def MockTopicData():
 
 #Classify documents into binary class
 # Run for EARN vs NOT EARN and GRAIN vs NOT GRAIN, recursively
-def DocumentClassification():
-
+def DocumentClassification(gamma_matrix, tf, labels_1_array, num_runs = 3):
+    print("*** DOCUMENT CLASSIFICATION ***")
+    
     # initialize the svm parameters for grid search and run both linear and radial basis function as kernels
-    parameters = {'kernel': ('linear','rbf'), 'C': [1,3,5,7,10], 'gamma':[0.01,0.05,0.10,0.3,0.5]}
-
+    #parameters = {'kernel': ('linear','rbf'), 'C': [1,3,5,7,10], 'gamma':[0.01,0.05,0.10,0.3,0.5]}
+    parameters = {'C': [1,3,5,7,10]}
+    
     # proportion of test data
     test_data_size = [0.01 ,0.05 ,0.1, 0.15, 0.2, 0.25]
-
+    
+    print("*** LDA gamma features ***")
     ## building classifier for low dimension data D(8000) X K(50) (for LDA features)
+    mean_stats_low_dim, sd_stats_low_dim = trainSVMs(num_runs, gamma_matrix, labels_1_array, parameters, test_data_size)
+    
+    if True:
+      print("*** Word features ***")
+      ### building a classifier for high dimension data D(8000) X w (for word feature)
+      mean_stats_high_dim, sd_stats_high_dim = trainSVMs(num_runs, tf, labels_1_array, parameters, test_data_size)
+    else:
+      mean_stats_high_dim, sd_stats_high_dim = np.nan, np.nan
+
+    return (mean_stats_low_dim, sd_stats_low_dim, mean_stats_high_dim, sd_stats_high_dim)
+    
+def trainSVMs(num_runs, data_array, labels_1_array, parameters, test_data_size):
     low_dim_acc = []
-    for k in range(0,6):
+    for k in range(num_runs):
+        print("*** Run %d ***" % k)
         acc_list_low_dim = []
         for i in test_data_size:
-            X_topic_train, X_topic_test, y_topic_train, y_topic_test = train_test_split(gamma_matrix, labels_1_array, test_size = i, random_state = k)
-            svr = svm.SVC()
+            print("*** test data size %f ***" % i)
+            X_topic_train, X_topic_test, y_topic_train, y_topic_test = train_test_split(data_array, labels_1_array, test_size = i, random_state = k)
+            svr = svm.LinearSVC()
             grid = GridSearchCV(svr, parameters)
             grid.fit(X_topic_train, y_topic_train)
             predicted = grid.predict(X_topic_test)
             acc_list_low_dim.append(accuracy_score(y_topic_test, predicted))
         low_dim_acc.append(acc_list_low_dim)
-    accuracy_low_dim = np.array(low_dim_acc) * 100
+    accuracy_low_dim = np.array(low_dim_acc)
     mean_stats_low_dim = np.mean(accuracy_low_dim, axis=0)
     sd_stats_low_dim = np.std(accuracy_low_dim, axis=0)
+    
+    return mean_stats_low_dim, sd_stats_low_dim
     #print (sd_stats_low_dim)
-
-
-    ### building a classifier for high dimension data D(8000) X w (for word feature)
-    high_dim_acc = []
-    for k in range(0,6):
-        acc_list_high_dim = []
-        for i in test_data_size:
-            X_word_train, X_word_test, y_word_train, y_word_test = train_test_split(tf, labels_1_array, test_size = i, random_state = k)
-            svr = svm.SVC()
-            grid = GridSearchCV(svr, parameters)
-            grid.fit(X_word_train, y_word_train)
-            predicted = grid.predict(X_word_test)
-            acc_list_high_dim.append(accuracy_score(y_word_test, predicted))
-        high_dim_acc.append(acc_list_high_dim)
-    accuracy_high_dim = np.array(high_dim_acc) * 100
-    mean_stats_high_dim = np.mean(accuracy_high_dim, axis=0)
-    sd_stats_high_dim = np.std(accuracy_high_dim, axis=0)
-    #print (sd_stats_high_dim)
-
-    return (mean_stats_low_dim, sd_stats_low_dim, mean_stats_high_dim, sd_stats_high_dim)
+    
+    
 
 # Get statistics (mean and standard deviation) for plotting
 #mean_low_dim, sd_low_dim,  mean_high_dim, sd_high_dim = DocumentClassification()
 
 # Training data - accuracy plot (fig. 10)
-def plotSVMModelAccuracy():
+def plotSVMModelAccuracy(mean_low_dim, sd_low_dim, mean_high_dim, sd_high_dim):
     #scatter plot for LDA features
     plt.errorbar(np.array([0.01 ,0.05 ,0.1, 0.15, 0.2, 0.25]), mean_low_dim,
                  yerr = sd_low_dim, label="LDA features", fmt="s--", linewidth=1)
     #scatter plot for word features
     plt.errorbar(np.array([0.01 ,0.05 ,0.1, 0.15, 0.2, 0.25]), mean_high_dim, yerr= sd_high_dim, label="Word features", fmt="s-", linewidth=1)
     plt.rc('axes', labelsize = 15)
-    plt.ylim(70, 100)
+    plt.ylim(0.7, 1.0)
     plt.xlim(0,0.3)
     plt.xlabel('Proportion of data used for training')
     plt.ylabel('Accuracy')
